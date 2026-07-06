092a6a0a544b62e456494acd277faed980cfa3f5
Aditya Pratap Singh <adityapratapsinghthb@gmail.com>
2026-07-06 22:37:06 +0530

    Add solution for Course Schedule II

---
+++ Course Schedule II/solution.cpp
@@ -0,0 +1,16 @@
+}
+        }
+        if(ans.size() == n) {
+                }
+                if(inDegree[i] == 0) {
+                    q.push(i);
+            for(auto i : adjList[frontNode]) {
+                inDegree[i]--;
+            ans.push_back(frontNode);
+
+            q.pop();
+            return ans;
+        }
+        else {
+            return {};
+        }
