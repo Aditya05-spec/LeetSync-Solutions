b54c8046e35a5f9eed21471d669ee0e70a34ca15
Aditya Pratap Singh <adityapratapsinghthb@gmail.com>
2026-07-07 15:28:44 +0530

    Add solution for Word Ladder

---
+++ Word Ladder/solution.cpp
@@ -0,0 +1,15 @@
+if(currWord == endWord) {
+
+            int currCount = frontNode.second;
+            string currWord = frontNode.first;
+            
+            q.pop();
+        st.erase(beginWord);
+        while(!q.empty()) {
+            pair<string , int> frontNode = q.front();
+        unordered_set<string> st(wordList.begin() , wordList.end());
+        q.push({beginWord , 1});
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string , int> > q;
+public:
+class Solution {
